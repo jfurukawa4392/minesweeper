1f9b89f521f9d3e7609b00125a1b0feb83139ed5
Jesse Furukawa <tuc64377@temple.edu>
2017-04-26 20:55:21 -0700

    add arg-parser scraping

---
+++ curb_testing.rb
@@ -0,0 +1,36 @@
+require 'curb'
+require 'arg-parser'
+
+
+class CurbParser
+
+    include ArgParser::DSL
+
+    purpose <<-EOT
+        Purpose: To pass triplebyte
+    EOT
+
+    positional_arg :url, 'This is the URL positional arg'
+    keyword_arg :method, 'method kwarg, default is GET',
+        default: 'GET'
+
+    keyword_arg :val_opt, 'This is a keyword arg with optional value',
+        value_optional: 'maybe'
+
+    flag_arg :save, 'save response to desktop'
+    rest_arg :file_names, 'This is where we specify that remaining args will be collected in an array'
+
+
+    def run
+        if opts = parse_arguments
+            # Do something with opts.my_positional_arg, opts.my_keyword_arg,
+            # opts.flag, and opts.files
+            # ...
+        else
+            show_help? ? show_help : show_usage
+        end
+    end
+
+end
+
+CurbParser.new.run
